4047836efe641268c8f74c13fcbf17c74bbbe209
mojtabaakbari <mojtaba.akbari.221b@gmail.com>
2024-11-27 10:37:48 +0330

    adding shared kernel as a dependency

--- {{ cookiecutter.project_folder_name }}/src/Api/GlobalUsing.cs
+++ {{ cookiecutter.project_folder_name }}/src/Api/GlobalUsing.cs
@@ -3,3 +3,4 @@ global using {{ cookiecutter.project_name.title() }}.Application;
 global using Microsoft.OpenApi.Models;
 global using {{ cookiecutter.project_name.title() }}.Api;
 global using DotNetEnv;
+global using SharedKernel;
